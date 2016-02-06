ba3f97c5f531582e1b620ba71a4dad454cc7f790
AdolfoRoch <adolfoh.f@hotmail.com>
2016-02-05 22:37:03 -0200

    inicio game
    base

---
+++ Game/Assets/cube.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube : MonoBehaviour {
+
+	public float moveSpeed;
+	private Vector3 m_CamForward;             // The current forward direction of the camera
+	private Vector3 m_Move;
+
+	// Use this for initialization
+	void Start ()
+	{
+		 moveSpeed = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//print(Input.GetAxis("Horizontal"));
+		transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+
+	}
+}
